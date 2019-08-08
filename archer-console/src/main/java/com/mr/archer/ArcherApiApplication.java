9befa97bdd0fa35448e74094b59dbc99ef1b8e51
jiang.feng <jiang.feng@huifu.com>
2019-08-08 18:22:32 +0800

    save

--- archer-console/src/main/java/com/mr/archer/ArcherApiApplication.java
+++ archer-console/src/main/java/com/mr/archer/ArcherApiApplication.java
@@ -2,10 +2,19 @@ package com.mr.archer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ArcherApiApplication {
+public class ArcherApiApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ArcherApiApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 设置启动类,用于独立tomcat运行的入口
+        return builder.sources(ArcherApiApplication.class);
+    }
+
 }
